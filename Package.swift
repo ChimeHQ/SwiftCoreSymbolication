175db3894eaf86559dc9b2527f55ca647efd7a41
Matt <85322+mattmassicotte@users.noreply.github.com>
2021-06-10 05:54:19 -0400

    Some basic sources and a test

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftCoreSymbolication",
+    platforms: [.macOS(.v10_15)],
+    products: [
+        .library(name: "SwiftCoreSymbolication", targets: ["SwiftCoreSymbolication"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/stacksift/CoreSymbolication", from: "0.1.0"),
+    ],
+    targets: [
+        .target(
+            name: "SwiftCoreSymbolication",
+            dependencies: ["CoreSymbolication"]
+        ),
+        .testTarget(
+            name: "SwiftCoreSymbolicationTests",
+            dependencies: ["SwiftCoreSymbolication"],
+            resources: [
+                .copy("Resources"),
+            ],
+            linkerSettings: [
+                .unsafeFlags([
+                    "-Xlinker", "-F",
+                    "-Xlinker", "/System/Library/PrivateFrameworks",
+                    "-Xlinker", "-framework",
+                    "-Xlinker", "CoreSymbolication",
+                ]),
+            ]
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
+)
---
+++ Tests/SwiftCoreSymbolicationTests/SwiftCoreSymbolicationTests.swift
@@ -0,0 +1,19 @@
+import XCTest
+@testable import SwiftCoreSymbolication
+
+final class CoreSymbolicationTests: XCTestCase {
+    func testSymbolicatorInterate() throws {
+        let dSYMURL = Bundle.module.url(forResource: "InlineTest.app", withExtension: "dSYM")
+        let url = try XCTUnwrap(dSYMURL?.appendingPathComponent("Contents/Resources/DWARF/InlineTest"))
+
+        let symbolicators = Symbolicator.symbolicators(at: url)
+
+        XCTAssertEqual(symbolicators.count, 1)
+
+        let owners = symbolicators.flatMap({ $0.symbolOwners })
+
+        XCTAssertEqual(owners.count, 1)
+
+        XCTAssertEqual(owners[0].uuid, UUID(uuidString: "62450421-5420-3AF0-87CA-010FC8D6CEBA"))
+    }
+}
